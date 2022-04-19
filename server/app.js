63d747354477db97749fa235ee4ad13148a54d1e
Alejandro Barrios <josebarriosrodas@gmail.com>
2022-04-19 03:59:50 -0600

    DB Fixed

--- server/app.js
+++ server/app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
 
+
+app.set('appName','JuegoMaya');
+app.set('port',3000);
+app.set('view engine','ejs');
+
+
+//middlewares
 app.use(express.json());
 
-
-
 app.get('/', (req, res) => {
-    res.send("Hello world!");
+    const data = [
+        {name:"test1"},
+        {name:"test2"},
+    ];
+    res.render('index.ejs',{personas: data});
 })
+
 
 app.post('/login', (req, res) => {
     console.log(req.body);
     res.send("<b>Login</b>");
 })
 
-app.listen(3000,() => {
-    console.log('listening on port 3000');
+//app.use(express.static('public'));
+
+app.listen(app.get('port'),() => {
+    console.log(app.get('appName'));
+    console.log('listening on port',app.get('port'));
 });
+
