2d0611dee53825cf8cbab835ab27c21ace98dce6
Alejandro Barrios <josebarriosrodas@gmail.com>
2022-04-21 11:57:13 -0600

    added cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 //middlewares
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 
 //config
 app.set('port',3000);
